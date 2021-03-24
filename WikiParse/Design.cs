b30940b80f719cb1d08533efe2ca1284e851c18c
NicholasWhit3 <64132625+NicholasWhit3@users.noreply.github.com>
2021-03-24 16:57:24 +0100

    Add files via upload

---
+++ WikiParse/Design.cs
@@ -0,0 +1,60 @@
+﻿namespace WikiParse
+{
+    partial class WikiParse
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if(disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+        #region Windows Form Designer generated code
+
+        private void InitializeCompent()
+        {
+            this.Input = new System.Windows.Forms.TextBox();
+            this.Output = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // Input
+            this.Input.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.Input.Font = new System.Drawing.Font("Arial", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Input.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
+            this.Input.Location = new System.Drawing.Point(24, 34);
+            this.Input.Name = "Input";
+            this.Input.Size = new System.Drawing.Size(800, 20);
+            this.Input.TabIndex = 0;
+            this.Input.Text = "Type here";
+            this.Input.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Input_KeyDown);
+            // Output
+            this.Output.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.Output.Location = new System.Drawing.Point(24, 78);
+            this.Output.Multiline = true;
+            this.Output.Name = "Output";
+            this.Output.Size = new System.Drawing.Size(800, 480);
+            this.Output.TabIndex = 2;
+            this.Output.Text = "Type something to get result.";
+            // Wiki
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(900, 600);
+            this.Controls.Add(this.Output);
+            this.Controls.Add(this.Input);
+            this.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "WikiParse";
+            this.Text = "WikiParse";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+
+
+        private System.Windows.Forms.TextBox Input;
+        private System.Windows.Forms.TextBox Output;
+    }
+}
---
+++ WikiParse/Form.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Xml;
+using System.Text.RegularExpressions;
+
+namespace WikiParse
+{
+    public partial class WikiParse : Form
+    {
+        public WikiParse()
+        {
+            InitializeCompent();
+        }
+        private void Input_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                try
+                {
+                    var webclient = new WebClient();
+                    string url = "https://en.wikipedia.org/w/api.php?format=xml&action=query&prop=extracts&titles=" + Input.Text + "&redirects=true";
+                    var pagesource = webclient.DownloadString(url);
+
+                    XmlDocument doc = new XmlDocument();
+                    doc.LoadXml(pagesource);
+                    var fnode = doc.GetElementsByTagName("extract")[0];
+                    string ss = fnode.InnerText;
+                    Regex regex = new Regex("\\<[^\\>]*\\>");
+                    string.Format("Before:{0}", ss);
+                    ss = regex.Replace(ss, string.Empty);
+
+                    string result = string.Format(ss);
+
+                    Output.Text = result;
+                }
+                catch
+                {
+
+                }
+            }
+        }
+    }
+}
